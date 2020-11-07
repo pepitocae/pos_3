be30c9c14cd21a0ed0c5cffb6f4007f3d8657dbc
Pepe Villalonga <pepitocae1959@gmail.com>
2020-11-06 19:47:33 -0600

    sistema 3 web 6-11-2020_19.46

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2019 <a href="https://www.mttechnology.mtcorp.mx" target="_blank">MT Technology</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
